146acfc181eddda61dfc62746c211356324c33f7
MikeNats <michalis.tsougkranis@gmail.com>
2014-09-03 22:16:16 +0100

    MVC Responsive Carousel

--- js/models/nodeModel.js
+++ js/models/nodeModel.js
@@ -2,6 +2,10 @@
 var app = app || {};
 
 app.nodeModel = Backbone.Model.extend({ 
+  url: "youForgetToIncludeLink,
+  title: "This is the default pic",
+  imgTitle: "This is the default pic",
+  src: "img/placeholder.jpg"
 
 
 
